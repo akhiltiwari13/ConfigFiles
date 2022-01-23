485cf7afb29ab8e9b39cf2fedd33c733800031cf
akhil <akhiltiwari.13@gmail.com>
2022-01-23 20:18:22 +0530

    set default fzf search to include hidden files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-#
 # my utility scripts.
 
 alias cdf='cd ~/files/'
 alias cdprj='cd ~/files/projects/'
 alias ll='ls -lart'
 
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 
