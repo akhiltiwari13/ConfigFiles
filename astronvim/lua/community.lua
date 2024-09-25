75422e632f286a9953baa8bdd809d9cec8c032e7
akhil <akhiltiwari.13@gmail.com>
2024-09-25 16:48:15 +0530

    [astronvim] add markdown related community plugs

--- astronvim/lua/community.lua
+++ astronvim/lua/community.lua
@@ -23,6 +23,8 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
 
+  -- { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.vim-matchup" },
 }
