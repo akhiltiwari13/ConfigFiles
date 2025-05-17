ae179e88ad3e880e8a13ad932c3a823253ec66cb
akhil <akhiltiwari.13@gmail.com>
2025-05-17 14:47:08 +0530

    [wezterm] minor font changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,11 +5,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
 -- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "tokyonight"
 config.initial_cols = 65
 config.initial_rows = 45
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
-config.font_size = 12.5
+config.font_size = 10.5
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.enable_scroll_bar = true
