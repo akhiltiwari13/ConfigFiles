5de69e36cd51764367f3b6780fe28f820ea0c034
akhil <akhiltiwari@quomptrade.com>
2025-09-15 16:58:43 +0530

    [wezterm] minor config changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,100 @@
--- Pull in the wezterm API
+-- Pull in the wezterm APIwez
 local wezterm = require("wezterm")
 local act = wezterm.action
+
 local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
-wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
 
-local config = require("config")
+-- local config = require("config")
 
-local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
-workspace_switcher.apply_to_config(config)
+local config = {
+	-- default_cursor_style = "SteadyBar",
+	automatically_reload_config = true,
+	-- window_close_confirmation = "NeverPrompt",
+	adjust_window_size_when_changing_font_size = false,
+	-- window_decorations = "RESIZE",
+	check_for_updates = true,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = false,
+	enable_scroll_bar = true,
+	hide_tab_bar_if_only_one_tab = false,
+	use_dead_keys = false,
+	debug_key_events = true,
+	default_workspace = "wezy",
+	-- font_size = 11.6,
+	font_size = 9.3,
+	-- font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
+	font = wezterm.font("Cascadia Mono", { weight = "Bold" }),
+	color_scheme = "tokyonight",
+	-- color_scheme = "tokyonight-day",
+	window_padding = {
+		left = 3,
+		right = 3,
+		top = 0,
+		bottom = 0,
+	},
+	background = {
+		{
+			source = {
+				Color = "#000000",
+			},
+			-- attachment = { Parallax = 0.0 },
+			width = "100%",
+			height = "100%",
+			opacity = 0.90,
+		},
+	},
+	-- from: https://akos.ma/blog/adopting-wezterm/
+	hyperlink_rules = {
+		-- Matches: a URL in parens: (URL)
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in brackets: [URL]
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in curly braces: {URL}
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in angle brackets: <URL>
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Then handle URLs not wrapped in brackets
+		{
+			-- Before
+			--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+			--format = '$0',
+			-- After
+			regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- implicit mailto link
+		{
+			regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+			format = "mailto:$0",
+		},
+	},
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+}
+-- config definition
+
 config.keys = {
 	{
 		key = "|",
 		mods = "LEADER",
+		-- mods = tostring(config.leader),
 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
 	{
@@ -19,25 +102,25 @@ config.keys = {
 		mods = "LEADER",
 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
-	-- {
-	-- 	key = "w",
-	-- 	mods = "LEADER",
-	-- 	action = wezterm.action_callback(function(win, pane)
-	-- 		resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-	-- 	end),
-	-- },
-	-- {
-	-- 	key = "W",
-	-- 	mods = "LEADER",
-	-- 	action = resurrect.window_state.save_window_action(),
-	-- },
+	{
+		key = "w",
+		mods = "LEADER",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+		end),
+	},
+	{
+		key = "W",
+		mods = "LEADER",
+		action = resurrect.window_state.save_window_action(),
+	},
 	{
 		key = "t",
 		mods = "LEADER",
 		action = resurrect.tab_state.save_tab_action(),
 	},
 	{
-		key = "s",
+		key = "x",
 		mods = "LEADER",
 		action = wezterm.action_callback(function(win, pane)
 			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
@@ -90,23 +173,16 @@ config.keys = {
 			mode = "Activate",
 		}),
 	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.PaneSelect({
-			mode = "Activate",
-		}),
-	},
-	{
-		key = "s",
-		mods = "LEADER",
-		action = workspace_switcher.switch_workspace(),
-	},
-	{
-		key = "S",
-		mods = "LEADER",
-		action = workspace_switcher.switch_to_prev_workspace(),
-	},
+	-- {
+	-- 	key = "s",
+	-- 	mods = "LEADER",
+	-- 	action = workspace_switcher.switch_workspace(),
+	-- },
+	-- {
+	-- 	key = "S",
+	-- 	mods = "LEADER",
+	-- 	action = workspace_switcher.switch_to_prev_workspace(),
+	-- },
 }
 
 -- tab navigation settings
@@ -124,4 +200,10 @@ for i = 1, 8 do
 	})
 end
 
+wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+workspace_switcher.apply_to_config(config)
+
+-- print("printing config thus far")
+-- print(config)
 return config
