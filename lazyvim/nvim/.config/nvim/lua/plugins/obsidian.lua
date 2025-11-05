b3307eb883b68a71a5702c97bdd55e3f60050fcb
akhil tiwari <akhiltiwari.13@gmail.com>
2025-11-06 01:57:17 +0530

    config updates to lazyvim

---
+++ lazyvim/nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,24 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "v3.14.3", -- recommended, use latest release instead of latest commit
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  ---@module 'obsidian'
+  ---@type obsidian.confg
+  opts = {
+    workspaces = {
+      {
+        name = "my-zettelkasten",
+        path = "~/Work/notes",
+      },
+      picker = { "folke/snacks.nvim" },
+    },
+  },
+}
