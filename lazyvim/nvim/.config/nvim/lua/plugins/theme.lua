56db3ce0bd889b6c09a3fb808f48bb5ffe406fd4
akhil tiwari <akhiltiwari.13@gmail.com>
2026-07-07 20:01:59 +0530

    [lazyvim|hyprland] incremental updates
    add new plugins from lazyvim website (recomendations)
    incremental updates to hyprland

--- lazyvim/nvim/.config/nvim/lua/plugins/theme.lua
+++ lazyvim/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,8 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "tokyonight",
     },
   },
 }
